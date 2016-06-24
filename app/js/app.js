c3630df5906e4aaff7fe9272cf9efea2190998aa
Camila Vilarinho <camilaavilarinho@gmail.com>
2016-06-24 10:41:26 -0300

    Add json file and start using DI

--- app/js/app.js
+++ app/js/app.js
@@ -3,51 +3,12 @@
 /* App Module */
 var observatoryApp = angular.module('observatoryApp', []);
 
-observatoryApp.controller('AppController', function AppController($scope){
-  $scope.years = [
-    {
-      id: "2016",
-      scholarship: [
-        {
-          name: "UESPI",
-          city: "Teresina",
-          number: 15,
-          value: 70000
-
-        }, {
-          name: "UFPI",
-          city: "Teresina",
-          number: 20,
-          value: 100000
-        }
-      ]
-    }, {
-      id: "2015",
-      scholarship: [
-        {
-          name: "UESPI",
-          city: "Teresina",
-          number: 30,
-          value: 170000
-
-        }, {
-          name: "UFPI",
-          city: "Teresina",
-          number: 20,
-          value: 100000
-        }
-      ]
-    }, {
-      id: "2014"
-    }, {
-      id: "2013"
-    }, {
-      id: "2012"
-    }, {
-      id: "2011"
-    }
-  ];  
-  $scope.data = $scope.years[0];
+observatoryApp.controller('AppController', function AppController($scope, $http){
+  $scope.years = [];
+  $http.get('json/years.json').then(function(response){
+    $scope.years = response.data;
+    $scope.data = $scope.years[0];
+  });
   $scope.calculateTotal = function(data){
     var total = 0;
     for(var i =0 ; i< data.length; i++){
