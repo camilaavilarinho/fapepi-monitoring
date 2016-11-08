eecd92b0609e9360d6787a42445507ce11a8e511
Camila Vilarinho <camilaavilarinho@gmail.com>
2016-11-08 10:03:14 -0300

    Updating

--- app/page-directive/page-directive.component.js
+++ app/page-directive/page-directive.component.js
@@ -109,61 +109,6 @@ angular.
           .on('mouseover', tip.show)
           .on('mouseout', tip.hide);
 
-
-          /*var width = d3.select(element[0]).node().offsetWidth - margin,
-          // calculate the height
-          height = scope.data.length * (barHeight + barPadding),
-          // Use the category20() scale function for multicolor support
-          color = d3.scale.category20(),
-          // our xScale
-          xScale = d3.scale.linear()
-          .domain([0, d3.max(data, function(d) {
-            return d.score;
-          })])
-          .range([0, width]);
-
-          // set the height based on the calculations above
-          svg.attr('height', height);
-
-          svg.append('text')
-          .attr("class", "title")
-          .text("Why Are We Leaving Facebook?");
-
-          //create a g element
-          svg.selectAll('svg')
-          .data(data).enter()
-          .append('g')
-          .attr('height', barHeight)
-          .attr('width', 140);
-
-          //create the rectangles for the bar chart
-          svg.selectAll('g')
-          //.data(data).enter()
-          .append('rect')
-          .attr('height', barHeight)
-          .attr('width', 140)
-          .attr('x', Math.round(margin/2))
-          .attr('y', function(d,i) {
-            return i * (barHeight + barPadding) + 10;
-          })
-          .attr('fill', function(d) { return color(d.score); })
-          .transition().ease("elastic")
-          .duration(1000)
-          .attr('width', function(d) {
-            return xScale(d.score);
-          });
-          //create a text element
-          svg.selectAll('g')
-          .append('text')
-          .attr('x', Math.round(margin/2))
-          .attr('y', function(d,i) {
-            return i * (barHeight + barPadding) + 25;
-          })
-          .attr("font-size", "15px")
-          //.attr("text-anchor", "middle")
-          .attr("fill", "#ddd")
-          .text(function(d) { return d.name + " (" + d.score + ")"});
-          */
         }
 
       }
