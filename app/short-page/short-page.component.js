fe10a606f5591e41128bebebaf5dd399542159ca
Camila Vilarinho <camilaavilarinho@gmail.com>
2016-11-30 11:24:18 -0300

    Creating a map directive - update

--- app/short-page/short-page.component.js
+++ app/short-page/short-page.component.js
@@ -1,93 +1,39 @@
 'use strict';
 
-// TODO: Use d3 with dependency injection
 angular.
-  module('shortPage', [])
-  .controller('Controller', ['$scope', function($scope) {
-    $scope.chartData = [
-      {name: "Greg", score: 98},
-      {name: "Ari", score: 96},
-      {name: 'Alguem', score: 75},
-      {name: "Teste", score: 18},
-      {name: "Camila", score: 28}
-    ];
-  }])
-  .directive('mapChart', function($parse) {
-    var directiveDefinitionObject = {
-
-      restrict: 'EA',
-
-      scope: {data: '='},
-      link: function (scope, element, attrs) {
-
-        var width = 660,
-        height = 800;
-
-        var svg = d3.select("#map").append("svg");
-
-
-        // Browser onresize event
-        window.onresize = function() {
-          scope.$apply();
-        };
-
-        // Watch for resize event
-        scope.$watch(function() {
-          return angular.element(window)[0].innerWidth;
-        }, function() {
-          scope.render(scope.data);
-        });
-
-        scope.$watch('data', function(newVals, oldVals) {
-          return scope.render(newVals);
-        }, true);
-
-        scope.render = function(data) {
-          // remove all previous items before render
-          svg.selectAll('*').remove();
-
-          d3.json("json/pi-micro.json", function(error, pi) {
-            if (error) return console.error(error);
-
-            var subunits = topojson.feature(pi, pi.objects.micro);
-
-            var projection = d3.geo.mercator()
-            .scale(3000)
-            .center([-40, -10])
-            .translate([width / 2, height / 2]);
-
-            var path = d3.geo.path()
-            .projection(projection);
-
-            var tip = d3.tip()
-            .attr('class', 'd3-tip')
-            .offset([-10, 0])
-            .html(function(d) {
-              return d.id;
-            })
-
-            svg.append("path")
-            .datum(subunits)
-            .attr("d", path);
-
-            svg.call(tip);
-
-            svg.selectAll(".subunit")
-            .data(topojson.feature(pi, pi.objects.micro).features)
-            .enter().append("path")
-            .attr("class", function(d) { return "subunit " + d.id; })
-            .attr("d", path)
-            .on('mouseover', tip.show)
-            .on('mouseout', tip.hide);
-          });
-
+  module('shortPage').
+  component('shortPage', {
+    templateUrl: 'short-page/short-page.template.html',
+    controller: ['$http', '$scope', function ShortPageController($http, $scope) {
+      /*var self = $scope;
+      self.years = [];
+      $http.get('json/summary.json').then(function(response){
+        self.years = response.data;
+        self.data = self.years[0];
+      });
+      self.calculateTotal = function(data){
+        var total = 0;
+        for(var i = 0 ; i < data.length; i++){
+          total+= data[i].number;
         }
-
-      }
-    };
-    return directiveDefinitionObject;
-
-    /*return {
-      templateUrl: 'page-directive/page-directive.template.html'
-    };*/
+        return total;
+      };
+      self.calculateValue = function(data){
+        var total = 0;
+        //console.log(data);
+        for(var i = 0 ; i < data.length; i++){
+          total+= data[i].value;
+        }
+        return total;
+      };
+      self.calculateSchool = function(data){
+        var total = 0;
+        for(var i = 0 ; i < data.length; i++){
+          if(data[i].name == "UESPI"){
+            total+= data[i].value;
+          }
+        }
+        return total;
+      }*/
+    }]
   });
